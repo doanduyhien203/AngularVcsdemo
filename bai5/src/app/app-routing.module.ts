f01968e898ed01999b2318cef6c872281b15e99c
doanduyhien203 <doanhien203302@gmail.com>
2022-08-26 17:54:27 +0700

    saf

--- bai5/src/app/app-routing.module.ts
+++ bai5/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
+   
   {
     path: '',
     component: HomeComponent,
+  },
+  
+  {
+    path: 'sign-in',
+    component: SignInComponent,
   },
   {
     path: 'article',
--- bai5/src/app/app.module.ts
+++ bai5/src/app/app.module.ts
@@ -4,12 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule],
-  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    FormsModule
+  ],
+
+  declarations: [AppComponent, HomeComponent, SignInComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
